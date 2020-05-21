9a963e350d3915788f078bbffedd56e926f84c12
okdistribute <633012+okdistribute@users.noreply.github.com>
2020-05-20 23:10:28 -0700

    fix: add fixes after testing

--- index.js
+++ index.js
@@ -18,67 +18,70 @@ var client = new Client({
 
 const MATRIX_ROOM_ID = config.matrix.room
 const CABAL_CHANNEL = config.cabal.channel
+console.log(config.cabal.nickname, 'listening on', CABAL_CHANNEL, MATRIX_ROOM_ID)
 
-var pendingCabals = config.cabal.keys.map(client.addCabal.bind(client))
-Promise.all(pendingCabals).then(() => {
-  var keys = client.getCabalKeys()
-  keys.map((key) => {
-    var cabal = client._getCabalByKey(key)
-    cabal.publishNick(config.cabal.nickname, noop)
-    var details = client.getDetails(cabal)
-    details.on('new-message', ({ channel, author, message }) => {
-      console.log(channel, author, message)
-      if (channel !== CABAL_CHANNEL) return
-      console.log('boot')
-      if (author !== 'UCABALBOT') {
-        var intent = bridge.getIntent('@cabal_' + author + `:${config.matrix.homeserver_url}`)
-        intent.sendText(MATRIX_ROOM_ID, message)
-      }
-    })
+client.addCabal(config.cabal.key).then(() => {
+  console.log(config.cabal.key)
+  var cabal = client._getCabalByKey(config.cabal.key)
+  cabal.publishNick(config.cabal.nickname, noop)
+  var details = client.getDetails(cabal)
+  details.on('new-message', ({ channel, author, message }) => {
+    if (channel !== CABAL_CHANNEL) return
+    if (author.name !== config.cabal.nickname) {
+      var homeserver = config.matrix.homeserver_url.replace('https://', '')
+      var intent = bridge.getIntent('@cabal_' + author.name + `:${homeserver}`)
+      intent.sendText(MATRIX_ROOM_ID, message.value.content.text)
+    }
   })
+
+  setupMatrix()
+}).catch((err) => {
+  console.error(err)
 })
 
-new Cli({
-  registrationPath: 'cabal-registration.yaml',
-  generateRegistration: function (reg, callback) {
-    reg.setId(AppServiceRegistration.generateToken())
-    reg.setHomeserverToken(AppServiceRegistration.generateToken())
-    reg.setAppServiceToken(AppServiceRegistration.generateToken())
-    reg.setSenderLocalpart('cabalbot')
-    reg.addRegexPattern('users', '@cabal_.*', true)
-    callback(reg)
-  },
-  run: function (port, matrixConfig) {
-    bridge = new Bridge({
-      homeserverUrl: config.matrix.homeserver_url,
-      domain: 'localhost',
-      registration: 'cabal-registration.yaml',
-      controller: {
-        onUserQuery: function (queriedUser) {
-          return {} // auto-provision users with no additonal data
-        },
-        onEvent: function (request, context) {
-          var event = request.getData()
-          // replace with your room ID
-          console.log(event)
-          if (event.type !== 'm.room.message' || !event.content || event.room_id !== MATRIX_ROOM_ID) {
-            return
-          }
-          var username = event.user_id
-          client.publishMessage({
-            type: 'chat/text',
-            content: {
-              text: `(${username}) ${event.content.body}`,
-              channel: CABAL_CHANNEL
-            }
+function setupMatrix () {
+  new Cli({
+    registrationPath: 'cabal-registration.yaml',
+    generateRegistration: function (reg, callback) {
+      reg.setId(AppServiceRegistration.generateToken())
+      reg.setHomeserverToken(AppServiceRegistration.generateToken())
+      reg.setAppServiceToken(AppServiceRegistration.generateToken())
+      reg.setSenderLocalpart('cabalbot')
+      reg.addRegexPattern('users', '@cabal_.*', true)
+      callback(reg)
+    },
+    run: function (port, matrixConfig) {
+      bridge = new Bridge({
+        homeserverUrl: config.matrix.homeserver_url,
+        domain: 'localhost',
+        registration: 'cabal-registration.yaml',
+        controller: {
+          onUserQuery: function (queriedUser) {
+            return {} // auto-provision users with no additonal data
           },
-          function (err, res) {
-            if (err) console.error('Error: %s', err)
-            console.log('DONE')
-          })
+          onEvent: function (request, context) {
+            var event = request.getData()
+            // replace with your room ID
+            if (event.type !== 'm.room.message' || !event.content || event.room_id !== MATRIX_ROOM_ID) {
+              return
+            }
+            var username = event.user_id
+            var cabal = client._getCabalByKey(config.cabal.key)
+            var details = client.getDetails(cabal)
+            details.publishMessage({
+              type: 'chat/text',
+              content: {
+                text: `(${username}) ${event.content.body}`,
+                channel: CABAL_CHANNEL
+              }
+            },
+            function (err, res) {
+              if (err) console.error('Error: %s', err)
+            })
+          }
         }
-      }
-    })
-    bridge.run(port, matrixConfig)
-  }
-}).run()
+      })
+      bridge.run(port, matrixConfig)
+    }
+  }).run()
+}
